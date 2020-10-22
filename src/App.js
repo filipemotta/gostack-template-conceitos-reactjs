e0b78dce35e7f4ec7395ddbac56720e3cde52af2
Filipe A. Motta Braga <filipemotta@MacBook-Pro-de-Filipe.local>
2020-10-22 15:09:03 -0300

    add functions addRepo and removeRepo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+   
+  const [ repositories, setRepository ] = useState([]);
+
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepository(response.data);
+    })
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      title: 'Repositorio 1',
+      url: 'http://repositorio1.gogs.com/',
+      techs: ['ReactJS','React Native']
+    });
+    setRepository([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+
+    await api.delete(`/repositories/${id}`);
+    
+   // setRepository(...repositories, repository => { repository.id !== id })
+   setRepository(repositories.filter(repository => repository.id !== id))
+
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repository => (
+           <li key={repository.id}>
+             {repository.title}
+           <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
-        </li>
+          </li>))
+        }
       </ul>
-
+        
       <button onClick={handleAddRepository}>Adicionar</button>
+      
     </div>
   );
 }
